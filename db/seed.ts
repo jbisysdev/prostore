9b3c984837c3fb85834df4aabd07ccff1c4145f5
Jermaine Buffington <jbisysdev@gmail.com>
2024-12-29 23:38:32 -0500

    Add 'User' seed data.  Add 'bcrypt-ts' dependency.

--- db/seed.ts
+++ db/seed.ts
@@ -2,12 +2,22 @@ import { PrismaClient } from '@prisma/client';
 import sampleData from './sample-data';
 
 async function main() {
+    // CREATE "prisma" client...
     const prisma = new PrismaClient();
+    // DELETE Existing Data...
     await prisma.product.deleteMany();
-
+    await prisma.account.deleteMany();
+    await prisma.session.deleteMany();
+    await prisma.verificationToken.deleteMany();
+    await prisma.user.deleteMany();
+    // CREATE New "product" objects...
     await prisma.product.createMany({
         data: sampleData.products
-    })
+    });
+    // CREATE New "user" objects...
+    await prisma.user.createMany({
+        data: sampleData.users
+    });
 
     console.log('Database seeded successfully!');
 };
